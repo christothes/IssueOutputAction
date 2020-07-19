43d94742a581c9a77d329b99c0021888e3561648
Christopher Scott <chriss@microsoft.com>
2020-07-19 16:36:30 -0500

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,50 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as octokit from '@actions/github'
+import * as io from '@actions/io'
+import { wait } from './wait'
+import * as fs from 'fs'
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const milestone: string = core.getInput('milestone')
-    core.debug(`Filtering on milestone: ${milestone}.`)
+    try {
+      const milestone: string = core.getInput('milestone')
+      core.debug(`Filtering on milestone: ${milestone}..`)
 
-    const state: string = core.getInput('state')
-    core.debug(`Filtering on state: ${state}.`)
+      const state: string = core.getInput('state')
+      core.debug(`Filtering on state: ${state}.`)
 
-    const notmodifiedfor: string = core.getInput('notmodifiedfor')
-    core.debug(`Filtering on notmodifiedfor: ${notmodifiedfor}.`)
+      const notmodifiedfor: string = core.getInput('notmodifiedfor')
+      core.debug(`Filtering on notmodifiedfor: ${notmodifiedfor}.`)
 
+      io.mkdirP(path.join('.', 'issues'));
 
-    core.debug(new Date().toTimeString());
-    await wait(10);
-    core.debug(new Date().toTimeString());
+      const gh = octokit.getOctokit(core.getInput('repotoken'));
 
-    core.setOutput('time', new Date().toTimeString());
+      var i = await gh.search.issuesAndPullRequests({
+        q: `milestone:"${milestone}" repo:christothes/IssueOutputAction`
+      });
+
+      core.debug('search issues: (' + i.status.toString() + ') ' + i.data.total_count.toString() + ' results');
+      i.data.items.forEach(issue => {
+        core.debug(`${JSON.stringify(issue.html_url)}`);
+        fs.writeFileSync(path.join('.', 'issues', `${issue.number}.json`), JSON.stringify(issue))
+      });
+
+      var ms = await gh.issues.getMilestone({
+        milestone_number: 1,
+        owner: 'christothes',
+        repo: 'IssueOutputAction'
+      });
+
+      core.debug(ms.data.title);
+      core.debug(ms.data.open_issues.toString() + ' open issues.');
+      
+      await wait(10);
+    } catch (err)
+    {
+      core.debug(err);
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
