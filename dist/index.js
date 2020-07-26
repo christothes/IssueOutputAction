2a3e0d097ecd03dcb291e0fad7f0c18969a72a6b
Christopher Scott <chriss@microsoft.com>
2020-07-26 16:24:56 -0500

    dist

--- dist/index.js
+++ dist/index.js
@@ -49,6 +49,37 @@ module.exports =
 /************************************************************************/
 /******/ ({
 
+/***/ 13:
+/***/ (function(__unusedmodule, exports) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.wait = void 0;
+function wait(milliseconds) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise(resolve => {
+            if (isNaN(milliseconds)) {
+                throw new Error('milliseconds not a number');
+            }
+            setTimeout(() => resolve('done!'), milliseconds);
+        });
+    });
+}
+exports.wait = wait;
+
+
+/***/ }),
+
 /***/ 87:
 /***/ (function(module) {
 
@@ -373,6 +404,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(__webpack_require__(453));
+const wait_1 = __webpack_require__(13);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -383,7 +415,7 @@ function run() {
             const notmodifiedfor = core.getInput('notmodifiedfor');
             core.debug(`Filtering on notmodifiedfor: ${notmodifiedfor}.`);
             core.debug(new Date().toTimeString());
-            //await wait(10);
+            yield wait_1.wait(10);
             core.debug(new Date().toTimeString());
             core.setOutput('time', new Date().toTimeString());
         }
